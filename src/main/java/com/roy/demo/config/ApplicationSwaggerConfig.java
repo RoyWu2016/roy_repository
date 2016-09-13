c10404e22e144796e6e3ab7a71589d894546662c
Roy Wu <Roy Wu@ldj-PC>
2016-09-13 18:22:50 +0800

    fixed bugs of swagger config

--- src/main/java/com/roy/demo/config/ApplicationSwaggerConfig.java
+++ src/main/java/com/roy/demo/config/ApplicationSwaggerConfig.java
@@ -2,14 +2,22 @@ package com.roy.demo.config;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+@EnableWebMvc  
+@EnableSwagger2  
+//@ComponentScan(basePackages = {"com.roy.demo.controller"})  
+//@Configuration 
 public class ApplicationSwaggerConfig {
 	
 	private static final Logger LOGGER = Logger.getLogger(ApplicationSwaggerConfig.class);
@@ -23,5 +31,14 @@ public class ApplicationSwaggerConfig {
                 .paths(PathSelectors.any())//过滤的接口
                 .build();
     }
+    
+    private ApiInfo apiInfo() {  
+        return new ApiInfoBuilder()  
+                .title("Spring 中使用Swagger2构建RESTful APIs")  
+                .termsOfServiceUrl("http://blog.csdn.net/he90227")  
+                .contact("逍遥飞鹤")  
+                .version("1.1")  
+                .build();  
+    }  
 
 }
--- src/main/java/com/roy/demo/controller/UserController.java
+++ src/main/java/com/roy/demo/controller/UserController.java
@@ -5,16 +5,18 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.roy.demo.model.UserInfo;
 import com.roy.demo.service.UserService;
 
-@Controller
+import io.swagger.annotations.ApiOperation;
+
+@RestController
 @RequestMapping("/user")
 public class UserController {
 	
@@ -23,9 +25,10 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@ApiOperation(value = "获取用户列表", notes = "")  
 	@RequestMapping("/showInfo/{userId}")
 	public String showUserInfo(ModelMap modelMap, @PathVariable int userId){
-		logger.debug("-----------------------showUserInfo-----------------------");
+		logger.info("-----------------------showUserInfo-----------------------");
 		UserInfo userInfo = userService.getUserById(userId);
 		modelMap.addAttribute("userInfo", userInfo);
 		return "/user/showInfo";
@@ -33,7 +36,7 @@ public class UserController {
 	
 	@RequestMapping("/showInfos")
 	public @ResponseBody Object showUserInfos(){
-		logger.debug("-----------------------showUserInfos-----------------------");
+		logger.info("-----------------------showUserInfos-----------------------");
 		List<UserInfo> userInfos = userService.getUsers();
 		return userInfos;
 	}
