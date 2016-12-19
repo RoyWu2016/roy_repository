94cb676f5a0238d111407db95c1c0062f26d1fe6
Roy Wu <Roy Wu@ldj-PC>
2016-12-19 12:49:34 +0800

    finished commons data url loading from db

--- src/main/java/com/roy/demo/config/ServiceConfig.java
+++ src/main/java/com/roy/demo/config/ServiceConfig.java
@@ -3,13 +3,13 @@ package com.roy.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class ServiceConfig {
 	
-	@Value("${user.service.url}")
+//	@Value("${user.service.url}")
 	private String userServiceUrl;
 	
-	@Value("${court.service.url}")
+//	@Value("${court.service.url}")
 	private String logServiceUrl;
 
 	public String getUserServiceUrl() {
