24ff78eaa27ef69a98d3934baac64bfbb6855139
Roy Wu <Roy Wu@ldj-PC>
2016-12-26 14:58:59 +0800

    tranctional completed

--- src/main/java/com/roy/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/roy/demo/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import com.roy.demo.dao.UserInfoMapper;
 import com.roy.demo.model.UserInfo;
@@ -27,15 +28,20 @@ public class UserServiceImpl implements UserService {
 		return userInfoMapper.selectAll();
 	}
 
-	@Transactional(propagation = Propagation.REQUIRED)
+	@Transactional(propagation = Propagation.REQUIRED)//, rollbackFor = Exception.class
 	public int insert(UserInfo userInfo) {
 		// TODO Auto-generated method stub
 		int result = userInfoMapper.insert(userInfo);
-		String string = null;
-		if (string.equals("")) {
-			int i = 0;
+		try {
+			String string = null;
+			if (string.equals("")) {
+				int i = 0;
+			}
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
 		}
-		System.out.println(result);
 		return result;
 	}
 
