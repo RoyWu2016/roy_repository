4d49f3815652270a3689dbdd04b5180514466865
Roy Wu <Roy Wu@ldj-PC>
2017-04-07 18:06:56 +0800

    commit static proxy

---
+++ src/main/java/com/roy/demo/pattern/proxy/static_proxy/IUserDao.java
@@ -0,0 +1,5 @@
+package com.roy.demo.pattern.proxy.static_proxy;
+
+public interface IUserDao {
+	void save();
+}
---
+++ src/main/java/com/roy/demo/pattern/proxy/static_proxy/UserDao.java
@@ -0,0 +1,10 @@
+package com.roy.demo.pattern.proxy.static_proxy;
+
+public class UserDao implements IUserDao {
+
+	public void save() {
+		// TODO Auto-generated method stub
+		System.out.println("----已经保存数据!----");
+	}
+
+}
